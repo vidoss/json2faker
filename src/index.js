6d9f7c3cb11c97e4c6d140f8b2fa9e6be73ed80e
Victor Doss <victor.doss@gmail.com>
2017-04-07 14:08:09 -0700

    Return approriate faker fn

--- src/index.js
+++ src/index.js
@@ -2,13 +2,37 @@
 const traverse = require('traverse');
 const prettier = require('prettier');
 const strEscape = require('js-string-escape');
+const fakerDefs = require('./definitions');
+const {filter} = require('fuzzaldrin');
 
 const prettierConfig = {
   printWidth: 128,
 };
 
 function genFaker(key, value) {
-  return key === '0' ? 'first' : key;
+  if (key !== '0') {
+    const match = filter(fakerDefs, key, {key: 'keyword', maxResults: 1});
+    if (match.length > 0) {
+      return match[0].fakerFn;
+    }
+  }
+
+  let fakerFn = 'lorem.sentence';
+  switch(typeof value) {
+    case 'boolean': {
+      fakerFn = 'random.boolean';
+      break;
+    }
+    case 'number': {
+      fakerFn = 'random.number';
+      break;
+    }
+    case 'string': {
+      fakerFn = value.indexOf(' ') === -1 ? 'random.word' : 'random.words';
+      break;
+    }
+  }
+  return fakerFn;
 }
 
 function json2faker(obj) {
